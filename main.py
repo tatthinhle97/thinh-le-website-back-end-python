31ad3304a141282f0ae565ab9b6da92ff7ff5455
Thinh Le <let9@go.stockton.edu>
2025-05-20 20:25:25 -0400

    Fix CORS

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ app = FastAPI()
 
 origins = [
     "http://localhost:5173",
-    "http://127.0.0.1:5173"
+    "http://127.0.0.1:5173",
+    "https://tatthinhle.vercel.app"
 ]
 
 app.add_middleware(

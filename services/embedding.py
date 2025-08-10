b2ac962c830f1acc9f432546c70ed8a7393f3483
Thinh Le <let9@go.stockton.edu>
2025-08-09 20:52:25 -0400

    Revert changes

--- services/embedding.py
+++ services/embedding.py
@@ -3,9 +3,7 @@ from sentence_transformers import SentenceTransformer
 # The PyTorch backend is the default backend for Sentence Transformers. If you
 # don’t specify a device, it will use the strongest available option across
 # “cuda”, “mps”, and “cpu”.
-model = SentenceTransformer(
-    "all-MiniLM-L6-v2",
-    backend="onnx")
+model = SentenceTransformer("all-MiniLM-L6-v2")
 
 def encode_texts(_sentences):
     return model.encode(_sentences)

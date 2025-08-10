97183b43f8b8da6478ad4a154d366b56e35bd51a
Thinh Le <let9@go.stockton.edu>
2025-08-10 09:55:54 -0400

    Remove chatbot dev

--- seedings/qdrant/index.py
+++ seedings/qdrant/index.py
@@ -1,49 +1,49 @@
-from seedings.qdrant.data.faq_metadata import FAQ_METADATA_POINTS
-from seedings.qdrant.data.page_metadata import PAGE_METADATA_POINTS
-from services.qdrant import create_collection, delete_collection, upsert
-from services.embedding import encode_texts
-from qdrant_client.models import PointStruct
-
-# Create a collection name website_metadata if not exists
-COLLECTION_NAME = "website_metadata"
-
-#delete_collection(COLLECTION_NAME)
-create_collection(COLLECTION_NAME)
-
-
-# Create a list of points
-points = []
-
-# Create points related to page metadata
-for page_metadata_point in PAGE_METADATA_POINTS:
-    metadata = page_metadata_point.payload.description
-    metadata_encoded = encode_texts(metadata)
-
-    points.append(
-        PointStruct(
-            id = page_metadata_point.id,
-            vector = metadata_encoded,
-            # Convert from python class object to dict type
-            payload = page_metadata_point.payload.__dict__
-        )
-    )
-
-# Create points related to faq metadata
-for faq_metadata_point in FAQ_METADATA_POINTS:
-    metadata = (
-        f"Questions: {faq_metadata_point.payload.question}. "
-        f"Answer: {faq_metadata_point.payload.answer}"
-    )
-    metadata_encoded = encode_texts(metadata)
-    points.append(
-        PointStruct(
-            id = faq_metadata_point.id,
-            vector = metadata_encoded,
-            # Convert from python class object to dict type
-            payload = faq_metadata_point.payload.__dict__
-        )
-    )
-
-
-#upsert(COLLECTION_NAME, points)
-
+# from seedings.qdrant.data.faq_metadata import FAQ_METADATA_POINTS
+# from seedings.qdrant.data.page_metadata import PAGE_METADATA_POINTS
+# from services.qdrant import create_collection, delete_collection, upsert
+# from services.embedding import encode_texts
+# from qdrant_client.models import PointStruct
+#
+# # Create a collection name website_metadata if not exists
+# COLLECTION_NAME = "website_metadata"
+#
+# #delete_collection(COLLECTION_NAME)
+# create_collection(COLLECTION_NAME)
+#
+#
+# # Create a list of points
+# points = []
+#
+# # Create points related to page metadata
+# for page_metadata_point in PAGE_METADATA_POINTS:
+#     metadata = page_metadata_point.payload.description
+#     metadata_encoded = encode_texts(metadata)
+#
+#     points.append(
+#         PointStruct(
+#             id = page_metadata_point.id,
+#             vector = metadata_encoded,
+#             # Convert from python class object to dict type
+#             payload = page_metadata_point.payload.__dict__
+#         )
+#     )
+#
+# # Create points related to faq metadata
+# for faq_metadata_point in FAQ_METADATA_POINTS:
+#     metadata = (
+#         f"Questions: {faq_metadata_point.payload.question}. "
+#         f"Answer: {faq_metadata_point.payload.answer}"
+#     )
+#     metadata_encoded = encode_texts(metadata)
+#     points.append(
+#         PointStruct(
+#             id = faq_metadata_point.id,
+#             vector = metadata_encoded,
+#             # Convert from python class object to dict type
+#             payload = faq_metadata_point.payload.__dict__
+#         )
+#     )
+#
+#
+# #upsert(COLLECTION_NAME, points)
+#
--- seedings/qdrant/model-evaluation.py
+++ seedings/qdrant/model-evaluation.py
@@ -1,19 +1,19 @@
-from sentence_transformers import SentenceTransformer, util
-
-# Load a pre-trained Sentence Transformer model
-model = SentenceTransformer('all-MiniLM-L6-v2')
-
-# Define the texts to compare
-user_query = "I want to search for projects on this website."
-projects_page = "This page contains all the projects on this website."
-home_page = "This page contains general information about the website."
-
-# Encode the sentences into embeddings
-embeddings = model.encode([user_query, projects_page, home_page])
-
-# Calculate cosine similarity between pairs of embeddings
-user_query_vs_projects_page = util.cos_sim(embeddings[0], embeddings[1])
-user_query_vs_home_page = util.cos_sim(embeddings[0], embeddings[2])
-
-print(f"Similarity between user's query and projects page description: {user_query_vs_projects_page.item():.4f}")
-print(f"Similarity between user's query and home page description: {user_query_vs_home_page.item():.4f}")
+# from sentence_transformers import SentenceTransformer, util
+#
+# # Load a pre-trained Sentence Transformer model
+# model = SentenceTransformer('all-MiniLM-L6-v2')
+#
+# # Define the texts to compare
+# user_query = "I want to search for projects on this website."
+# projects_page = "This page contains all the projects on this website."
+# home_page = "This page contains general information about the website."
+#
+# # Encode the sentences into embeddings
+# embeddings = model.encode([user_query, projects_page, home_page])
+#
+# # Calculate cosine similarity between pairs of embeddings
+# user_query_vs_projects_page = util.cos_sim(embeddings[0], embeddings[1])
+# user_query_vs_home_page = util.cos_sim(embeddings[0], embeddings[2])
+#
+# print(f"Similarity between user's query and projects page description: {user_query_vs_projects_page.item():.4f}")
+# print(f"Similarity between user's query and home page description: {user_query_vs_home_page.item():.4f}")
